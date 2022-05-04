967d6c5d8969c0f2ab38d0ce072abf45eeec3e1d
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2022-05-04 14:06:07 +0200

    Init

---
+++ src/DaikinCloudAirConditioningAccessory.ts
@@ -0,0 +1,126 @@
+import {Service, PlatformAccessory, CharacteristicValue} from 'homebridge';
+import {DaikinCloudPlatform} from './platform';
+
+export class DaikinCloudAirConditioningAccessory {
+    private service: Service;
+
+    constructor(
+        private readonly platform: DaikinCloudPlatform,
+        private readonly accessory: PlatformAccessory,
+    ) {
+
+        this.accessory.getService(this.platform.Service.AccessoryInformation)!
+            .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
+            .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
+            .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+
+        this.service = this.accessory.getService(this.platform.Service.HeaterCooler) || this.accessory.addService(this.platform.Service.HeaterCooler);
+
+        this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.getData('climateControl', 'name').value);
+
+        this.service.getCharacteristic(this.platform.Characteristic.Active)
+            .onSet(this.handleStateSet.bind(this))
+            .onGet(this.handleStateGet.bind(this));
+
+        this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+            .onGet(this.handleCurrentTemperatureGet.bind(this));
+
+        this.service.getCharacteristic(this.platform.Characteristic.TargetHeaterCoolerState)
+            .onGet(this.handleTargetHeaterCoolerStateGet.bind(this))
+            .onSet(this.handleTargetHeaterCoolerStateSet.bind(this));
+
+        this.service.getCharacteristic(this.platform.Characteristic.CoolingThresholdTemperature)
+            .onGet(this.handleCoolingThresholdTemperatureGet.bind(this))
+            .onSet(this.handleCoolingThresholdTemperatureSet.bind(this));
+
+        this.service.getCharacteristic(this.platform.Characteristic.HeatingThresholdTemperature)
+            .onGet(this.handleHeatingThresholdTemperatureGet.bind(this))
+            .onSet(this.handleHeatingThresholdTemperatureSet.bind(this));
+
+        setInterval(async () => {
+            await this.accessory.context.device.updateData();
+        }, 10000);
+    }
+
+    async handleStateSet(value: CharacteristicValue) {
+        this.platform.log.debug('Characteristic handleStateSet, value', value);
+        const state = value as boolean;
+        await this.accessory.context.device.setData('climateControl', 'onOffMode', state ? 'on' : 'off');
+        await this.accessory.context.device.updateData();
+    }
+
+    async handleStateGet(): Promise<CharacteristicValue> {
+        const state = await this.accessory.context.device.getData('climateControl', 'onOffMode').value;
+        this.platform.log.debug('Characteristic getOn, state ->', state);
+        return state === 'on';
+    }
+
+    async handleCurrentTemperatureGet(): Promise<CharacteristicValue> {
+        const temperature = await this.accessory.context.device.getData('climateControl', 'sensoryData', '/roomTemperature').value;
+        this.platform.log.debug('Characteristic handleCurrentTemperatureGet, temperature ->', temperature);
+        return temperature;
+    }
+
+    async handleCoolingThresholdTemperatureGet(): Promise<CharacteristicValue> {
+        const temperature = await this.accessory.context.device.getData('climateControl', 'temperatureControl', '/operationModes/cooling/setpoints/roomTemperature').value;
+        this.platform.log.debug('Characteristic handleCoolingThresholdTemperatureGet, temperature ->', temperature);
+        return temperature;
+    }
+
+    async handleCoolingThresholdTemperatureSet(value: CharacteristicValue) {
+        const temperature = value as number;
+        this.platform.log.debug('Characteristic handleCoolingThresholdTemperatureSet, temperature ->', temperature);
+        await this.accessory.context.device.setData('climateControl', 'temperatureControl', '/operationModes/cooling/setpoints/roomTemperature', temperature);
+        await this.accessory.context.device.updateData();
+    }
+
+    async handleHeatingThresholdTemperatureGet(): Promise<CharacteristicValue> {
+        const temperature = await this.accessory.context.device.getData('climateControl', 'temperatureControl', '/operationModes/heating/setpoints/roomTemperature').value;
+        this.platform.log.debug('Characteristic handleHeatingThresholdTemperatureGet, temperature ->', temperature);
+        return temperature;
+    }
+
+    async handleHeatingThresholdTemperatureSet(value: CharacteristicValue) {
+        const temperature = value as number;
+        this.platform.log.debug('Characteristic handleHeatingThresholdTemperatureSet, temperature ->', temperature);
+        await this.accessory.context.device.setData('climateControl', 'temperatureControl', '/operationModes/heating/setpoints/roomTemperature', temperature);
+        await this.accessory.context.device.updateData();
+    }
+
+    async handleTargetHeaterCoolerStateGet(): Promise<CharacteristicValue> {
+        const operationMode = await this.accessory.context.device.getData('climateControl', 'operationMode').value;
+        this.platform.log.debug('Characteristic handleTargetHeaterCoolerStateGet, operationMode ->', operationMode);
+
+        switch (operationMode) {
+            case 'cooling':
+                return this.platform.Characteristic.TargetHeaterCoolerState.COOL;
+            case 'heating':
+                return this.platform.Characteristic.TargetHeaterCoolerState.HEAT;
+            default:
+                return this.platform.Characteristic.TargetHeaterCoolerState.AUTO;
+        }
+    }
+
+    async handleTargetHeaterCoolerStateSet(value: CharacteristicValue) {
+        const operationMode = value as number;
+        this.platform.log.debug('Characteristic handleTargetHeaterCoolerStateSet, OperationMode ->', value);
+        let daikinOperationMode = 'cooling';
+
+        switch (operationMode) {
+            case this.platform.Characteristic.TargetHeaterCoolerState.COOL:
+                daikinOperationMode = 'cooling';
+                break;
+            case this.platform.Characteristic.TargetHeaterCoolerState.HEAT:
+                daikinOperationMode = 'heating';
+                break;
+            case this.platform.Characteristic.TargetHeaterCoolerState.AUTO:
+                daikinOperationMode = 'auto';
+                break;
+
+        }
+
+        this.platform.log.debug('Characteristic handleTargetHeaterCoolerStateSet, daikinOperationMode ->', daikinOperationMode);
+        await this.accessory.context.device.setData('climateControl', 'operationMode', daikinOperationMode);
+        await this.accessory.context.device.updateData();
+    }
+}
--- src/platform.ts
+++ src/platform.ts
@@ -1,116 +1,103 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+import {API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic} from 'homebridge';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import {PLATFORM_NAME, PLUGIN_NAME} from './settings';
+import {DaikinCloudAirConditioningAccessory} from './DaikinCloudAirConditioningAccessory';
 
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
-  public readonly Service: typeof Service = this.api.hap.Service;
-  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+import DaikinCloudController from 'daikin-controller-cloud';
+import path from 'path';
+import fs from 'fs';
 
-  // this is used to track restored cached accessories
-  public readonly accessories: PlatformAccessory[] = [];
+export class DaikinCloudPlatform implements DynamicPlatformPlugin {
+    public readonly Service: typeof Service = this.api.hap.Service;
+    public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
-  constructor(
-    public readonly log: Logger,
-    public readonly config: PlatformConfig,
-    public readonly api: API,
-  ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    public readonly accessories: PlatformAccessory[] = [];
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
-    this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
-    });
-  }
+    public readonly storagePath: string = '';
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
-  configureAccessory(accessory: PlatformAccessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
+    constructor(
+        public readonly log: Logger,
+        public readonly config: PlatformConfig,
+        public readonly api: API,
+    ) {
+        this.log.debug('Finished initializing platform:', this.config.name);
 
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
-  }
+        this.storagePath = api.user.storagePath();
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
-
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
-
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
-
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
-
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
+        this.api.on('didFinishLaunching', () => {
+            log.debug('Executed didFinishLaunching callback');
+            this.discoverDevices(this.config.username, this.config.password);
+        });
     }
-  }
+
+    configureAccessory(accessory: PlatformAccessory) {
+        this.log.info('Loading accessory from cache:', accessory.displayName);
+        this.accessories.push(accessory);
+    }
+
+    async discoverDevices(username: string, password: string) {
+        const daikinCloud = await this.initiateDaikinCloudController(username, password);
+        const devices = await daikinCloud.getCloudDevices();
+
+        if (devices.length === 0) {
+            this.log.info('No devices found');
+        }
+
+        devices.forEach(device => {
+            this.log.info('Device found with id: ' + device.getId() + ' Data:');
+            this.log.info('    name: ' + device.getData('climateControl', 'name').value);
+            this.log.info('    last updated: ' + device.getLastUpdated());
+            this.log.info('    modelInfo: ' + device.getData('gateway', 'modelInfo').value);
+
+            const uuid = this.api.hap.uuid.generate(device.getId());
+
+            const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+            if (existingAccessory) {
+                this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+                existingAccessory.context.device = device;
+                this.api.updatePlatformAccessories([existingAccessory]);
+                new DaikinCloudAirConditioningAccessory(this, existingAccessory);
+            } else {
+                this.log.info('Adding new accessory:', device.getData('climateControl', 'name').value);
+                const accessory = new this.api.platformAccessory(device.getData('climateControl', 'name').value, uuid);
+                accessory.context.device = device;
+                new DaikinCloudAirConditioningAccessory(this, accessory);
+                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+            }
+        });
+    }
+
+    async initiateDaikinCloudController(username, password) {
+        let tokenSet;
+        const options = {
+            logger: this.log.info,          // optional, logger function used to log details depending on loglevel
+            logLevel: 'info',               // optional, Loglevel of Library, default 'warn' (logs nothing by default)
+            proxyOwnIp: '192.168.xxx.xxx',  // required, if proxy needed: provide own IP or hostname to later access the proxy
+            proxyPort: 8888,                // required: use this port for the proxy and point your client device to this port
+            proxyWebPort: 8889,             // required: use this port for the proxy web interface to get the certificate and start Link for login
+            proxyListenBind: '0.0.0.0',     // optional: set this to bind the proxy to a special IP, default is '0.0.0.0'
+            proxyDataDir: this.storagePath, // Directory to store certificates and other proxy relevant data to
+            communicationTimeout: 10000,    // Amount of ms to wait for request and responses before timeout
+            communicationRetries: 3,        // Amount of retries when connection timed out
+        };
+
+        const tokenFile = path.join(this.storagePath, 'daikincloudtokenset.json');
+        this.log.info(`Write/read Daikin Cloud tokenset from ${tokenFile}`);
+
+        if (fs.existsSync(tokenFile)) {
+            this.log.debug(`Daikin Cloud tokenset found at ${tokenFile}`);
+            tokenSet = JSON.parse(fs.readFileSync(tokenFile).toString());
+        }
+
+        const daikinCloud = new DaikinCloudController(tokenSet, options);
+
+        daikinCloud.on('token_update', tokenSet => {
+            this.log.info(`UPDATED Daikin Cloud tokenset, use for future and wrote to ${tokenFile}`);
+            fs.writeFileSync(tokenFile, JSON.stringify(tokenSet));
+        });
+
+        await daikinCloud.login(username, password);
+
+        return daikinCloud;
+    }
 }
